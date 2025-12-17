42950b33e93bd44bfa94fd454ae7ed4df06c8221
DavidZarfati <zarda2001@gmail.com>
2025-12-17 08:51:48 +0100

    feat:creato array di post test

--- app.js
+++ app.js
@@ -3,10 +3,57 @@ import express from "express"
 const app = express()
 const port = 3000
 
+const posts = [
+    {
+        title: "I Am BatMan",
+        content: "storia di un pipistrello ubriaco",
+        path: "./BOH",
+        tags: ["animals", "heroes", "addictions"]
+    },
+    {
+        title: "I Am BatMan",
+        content: "storia di un pipistrello ubriaco",
+        path: "./BOH",
+        tags: ["animals", "heroes", "addictions"]
+    },
+    {
+        title: "I Am BatMan",
+        content: "storia di un pipistrello ubriaco",
+        path: "./BOH",
+        tags: ["animals", "heroes", "addictions"]
+    },
+    {
+        title: "I Am BatMan",
+        content: "storia di un pipistrello ubriaco",
+        path: "./BOH",
+        tags: ["animals", "heroes", "addictions"]
+    },
+    {
+        title: "I Am BatMan",
+        content: "storia di un pipistrello ubriaco",
+        path: "./BOH",
+        tags: ["animals", "heroes", "addictions"]
+    }
+]
+
+
+
 
 app.get("/", (req, res) => {
     console.log("sono console log 1");
     res.send("Server Del Mio Blog")
+})
+app.get("/bacheca", (req, res) => {
+    console.log("sono console log dei post");
+
+    const postList = posts.map((post) => {
+        title: post.title;
+        content: post.content;
+        imageUrl: post.path;
+        tags: post.tags;
+    })
+
+    res.json(postList)
 })
 
 
