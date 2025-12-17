4a26108ea417b9b59f3d5e840f8d2fbeed46c508
DavidZarfati <zarda2001@gmail.com>
2025-12-17 08:43:35 +0100

    feat;milestone 1

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-console.log('Ciao dal express-blog-intro')
+import express from "express"
+
+const app = express()
+const port = 3000
+
+
+app.get("/", (req, res) => {
+    console.log("sono console log 1");
+    res.send("Server Del Mio Blog")
+})
+
+
+
+
+
+
+app.listen(port, function () {
+    console.log("il server è in ascolto sulla porta " + port);
+})
